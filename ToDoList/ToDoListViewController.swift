1444b9662271468be4bf54391f429c1510bd70b8
Yashraj Gujar <yashraj.gujar4394@gmail.com>
2019-03-26 12:20:49 +0530

    Add New Functionality Completed

--- ToDoList/ToDoListViewController.swift
+++ ToDoList/ToDoListViewController.swift
@@ -11,20 +11,21 @@ import UIKit
 class ToDoListViewController: UITableViewController {
     
     
-    let itemArray = ["Search For iOS Internships","Eat Apples","Exercise for 15 mins","Take a nap for 15 mins"]
-
+    var itemArray = ["Search For iOS Internships","Eat Apples","Exercise for 15 mins","Take a nap for 15 mins"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         
+        
     }
-
     
-//MARK : TablevIew DataSource Methods -
+    
+    // MARK : TablevIew DataSource Methods -
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-         return 1
+        return 1
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -36,7 +37,7 @@ class ToDoListViewController: UITableViewController {
         
         return cell
         
-        //MARK - Table View Delegate Methods
+        // MARK-Table View Delegate Methods
     }
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 50
@@ -51,9 +52,36 @@ class ToDoListViewController: UITableViewController {
         }
         print(itemArray[indexPath.row])
         tableView.deselectRow(at: indexPath, animated: true)
-
+        
     }
-
+    
+    // MARK-Add New Items
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create New Item"
+            alertTextField.clearsOnInsertion = false
+            textField = alertTextField
+        }
+        let addAction = UIAlertAction(title: "Add", style: .default) { (action) in
+            
+            // What will happen once the user clicks  on add button on our UIAlert
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+            
+        }
+        
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addAction(addAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true, completion: nil)
+        
+    }
     
 }
 
